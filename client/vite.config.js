d1198018d3179cfd7aad311efa04309422b47312
MollyMai99 <miaoling.mai99@gmail.com>
2024-06-03 18:11:40 +0800

    feat:connect frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,19 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: resolve(__dirname), // 指定根目录为client文件夹
   plugins: [react()],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
   },
 });
